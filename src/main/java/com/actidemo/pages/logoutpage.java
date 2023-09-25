1db0c9d7fafc0f96bcb09af4426838066f7fe933
hari0 <hari0@sai2>
2023-09-25 12:16:56 -0400

    commited:added a logout page

---
+++ src/main/java/com/actidemo/pages/logoutpage.java
@@ -0,0 +1,29 @@
+package com.actidemo.pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import com.actidemo.base.driverscript;
+
+/**
+ * 
+ * This is logoutpage
+ * Developed by hari
+ * Date 09/25/2023
+ *
+ */
+public class logoutpage extends driverscript {
+	
+	@FindBy(id="logout") WebElement linklogout;
+	
+	
+	public logoutpage() {
+		PageFactory.initElements(driver, this);
+	}
+	
+	public void clicklogout() {
+		linklogout.click();
+	}
+
+}
